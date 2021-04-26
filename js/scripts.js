32951a2cc903d1462cce3f5a39b51bac122685ad
Charles Mwathi <mwathi.kinyua@gmail.com>
2021-04-26 11:07:37 +0300

    Add the form content scripts and the jquery library

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("form#groceriesForm").submit(function(event) {
+    event.preventDefault();
+    const fruits = $("select#selectFruits").val();
+    const meat = $("select#selectMeat").val();
+    const dairyProducts = $("select#selectDairyProducts").val();
+    const bakeryGoods = $("select#selectBakeryGoods").val();
+    const juice = $("select#selectJuice").val();
+
+    const array = [fruits, meat, dairyProducts, bakeryGoods, juice];
+    let sortedGroceries = [];
+    sortedGroceries = array.sort();
+    sortedGroceries= sortedGroceries.map(function(x) {
+      return x.toUpperCase();
+    });
+
+    $("#groceriesOutput").text(sortedGroceries) + '<br />';
+
+    $("#hidden").show();
+  });
+});
